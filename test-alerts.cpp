13700ec8c2a70c6afe3b4b89e04f0d383c777853
bkru1kor <s.kruthika@in.bosch.com>
2023-07-01 21:09:57 +0200

    Added testcases

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,4 +5,30 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(31, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+}
+
+TEST_CASE("classify Temperature Breach") {
+  BatteryCharacter batteryCharacter;
+  batteryCharacter.coolingType = PASSIVE_COOLING;
+  checkAndAlert(TO_CONTROLLER, batteryCharacter, 5);
+  checkAndAlert(TO_EMAIL, batteryCharacter, 5);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 5) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
+  
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 5) == NORMAL);
+  batteryCharacter.coolingType = HI_ACTIVE_COOLING;
+  checkAndAlert(TO_CONTROLLER, batteryCharacter, -1);
+  checkAndAlert(TO_EMAIL, batteryCharacter, -1);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46) == TOO_HIGH);
+
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 5) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
+  batteryCharacter.coolingType = MED_ACTIVE_COOLING;
+  checkAndAlert(TO_CONTROLLER, batteryCharacter, 41);
+  checkAndAlert(TO_EMAIL, batteryCharacter, 41);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 41) == TOO_HIGH);
 }
