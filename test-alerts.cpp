3681ee6c28c4b6e63ecbaba1c49a0ba3b8b7c986
Kruthika0919 <137186700+Kruthika0919@users.noreply.github.com>
2023-07-02 04:42:28 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -15,37 +15,19 @@ TEST_CASE("classify Temperature Breach") {
   checkAndAlert(TO_CONTROLLER, batteryCharacter, 5);
   checkAndAlert(TO_EMAIL, batteryCharacter, 5);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 5) == NORMAL);
-
-  checkAndAlert(TO_CONTROLLER, batteryCharacter, -1);
-  checkAndAlert(TO_EMAIL, batteryCharacter, -1);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
-
-  checkAndAlert(TO_CONTROLLER, batteryCharacter, 36);
-  checkAndAlert(TO_EMAIL, batteryCharacter, 36);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
   
   batteryCharacter.coolingType = HI_ACTIVE_COOLING;
-  checkAndAlert(TO_CONTROLLER, batteryCharacter, 5);
-  checkAndAlert(TO_EMAIL, batteryCharacter, 5);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 5) == NORMAL);
-  
   checkAndAlert(TO_CONTROLLER, batteryCharacter, -1);
   checkAndAlert(TO_EMAIL, batteryCharacter, -1);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW);
-
-  checkAndAlert(TO_CONTROLLER, batteryCharacter, 46);
-  checkAndAlert(TO_EMAIL, batteryCharacter, 46);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46) == TOO_HIGH);
 
   batteryCharacter.coolingType = MED_ACTIVE_COOLING;
-  checkAndAlert(TO_CONTROLLER, batteryCharacter, 5);
-  checkAndAlert(TO_EMAIL, batteryCharacter, 5);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 5) == NORMAL);
-
-  checkAndAlert(TO_CONTROLLER, batteryCharacter, -1);
-  checkAndAlert(TO_EMAIL, batteryCharacter, -1);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
-
   checkAndAlert(TO_CONTROLLER, batteryCharacter, 41);
   checkAndAlert(TO_EMAIL, batteryCharacter, 41);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 41) == TOO_HIGH);
